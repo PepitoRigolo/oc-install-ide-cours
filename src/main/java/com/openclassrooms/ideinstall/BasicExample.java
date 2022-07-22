755ab916e7579a8ef421829602351db42d0e9fce
PepitoRigolo <romain.ricard.dev@gmail.com>
2022-07-22 14:59:41 +0200

    Test

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -6,7 +6,7 @@ public class BasicExample {
 	int i;
 	for (i = 0; i <= 10; i++) {
 	    System.out.println("Hello bugs !");
-	    System.out.println("test" + i);
 	}
+	System.out.println("BOUMDD");
     }
 }
